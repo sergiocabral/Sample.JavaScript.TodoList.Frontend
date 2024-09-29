61d47cbf7b12ec96fea5132000282c3f7d904e30
Sergio Cabral <git@sergiocabral.com>
2024-09-29 11:08:56 -0300

    JS: Referência aos controles do layout

---
+++ src/public/script.js
@@ -0,0 +1,4 @@
+const elFiltro = document.getElementById('filtro');
+const elTarefas = document.getElementById('tarefas');
+const elNovaTarefaDescricao = document.getElementById('nova-tarefa');
+const elAdicionarNovaTarefa = document.getElementById('adicionar-nova-tarefa');
